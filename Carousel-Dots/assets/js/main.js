c02963e48f9105c73baa18d0f1d4ef1910327373
muhibbullahmasud <88580797+muhibbullahmasud@users.noreply.github.com>
2021-08-22 13:40:35 +0600

    Add files via upload

---
+++ Carousel-Dots/assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+    "use-strict";
+
+    $(document).ready(function () {
+        $(".client-testimonial-carousel").owlCarousel({
+           items: 4,
+           autoplay: false,
+           loop: true,
+           margin: 30,
+          dots: true,
+         nav: false,
+        });
+    }); 
+
+})(jQuery);
+
+
+
+
+
+
+
+
